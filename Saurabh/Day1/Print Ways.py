4fc4f7eee50df7175ea969f0b9525116e6108dcc
saurabhsarkar7 <saurabhsarkar7@gmail.com>
2024-06-14 20:45:29 +0530

    Updated with print methods

--- Saurabh/Day1/Print Ways.py
+++ Saurabh/Day1/Print Ways.py
@@ -1,4 +1,24 @@
+#The first program
 print("Hello World!")
+#Different ways to accomodate special characters in a print statement
 print("1. Add '3gm.' of salt")
 print('1. Add "3gm." of salt')
 print("1. Add \"3gm.\" of salt")
+#Print in new lines using \n operator
+print("Helo World!\nHello World!\nBye World!")
+#Concatenation
+print("Hello" + " " +"Saurabh"+ "!")
+#Input Function
+input("what is your name?")
+#Print with Input Function
+print("Hello " + input("What is your name?"))
+print(input())
+#Print with  variables
+num1 =int(input ())
+num2 = int(input())
+print(num1 * num2)
+#print len output
+A = len("Saurabh")
+print(A)
+#OR
+print(len(input()))
