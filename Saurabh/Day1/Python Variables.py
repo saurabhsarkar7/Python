a782e95a07fcfc7e16880af434def6dd3fd20ced
saurabhsarkar7 <saurabhsarkar7@gmail.com>
2024-06-14 21:16:23 +0530

    Variables

---
+++ Saurabh/Day1/Python Variables.py
@@ -0,0 +1,22 @@
+#Variable gets updated when we pass different values
+name="Reema"
+print(name)
+
+name='Saurabh'
+print(name)
+
+#Number Swapping
+a = input()
+b = input()
+
+c=a
+a=b
+b=c
+
+print("a: " +a)
+print("b: "+b)
+#variable Naming convention
+#No space in between
+#No number in beginning
+#No key words like input,print,etc.
+#Names are case sensitive, use the same case while calling them
