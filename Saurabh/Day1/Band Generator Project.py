79d84cc4d7509d8a2b4e2f8b82d81992f678247d
saurabhsarkar7 <saurabhsarkar7@gmail.com>
2024-06-15 13:32:46 +0530

    Variables

---
+++ Saurabh/Day1/Band Generator Project.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator Project")
+city = input("Which city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("your band name could be "+city+ " " +pet)
