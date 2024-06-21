ddf29e0e149f4d41dd022003f6454a8235a65ea6
saurabhsarkar7 <saurabhsarkar7@gmail.com>
2024-06-21 23:03:40 +0530

    Nested if - BMI

--- Saurabh/Day3/Nested if elif.py
+++ Saurabh/Day3/Nested if elif.py
@@ -25,18 +25,4 @@ else:
 #Different types of operator: >,>=,<,<=,!=,==(Double Equal to compare if the left and right value same).
 
 
-#BMI Calculator
-weight =int(input())
-height = float(input())
-bmi = weight/(height)**2
 
-if bmi <18.5:
-    print(f"{bmi}-underweight")
-elif  bmi<25:
-    print(f"{bmi}-normal weight")
-elif bmi<30:
-    print(f"{bmi}-slightly over weight")
-elif bmi <35:
-    print(f"{bmi}-obese")
-else:
-    print(f"{bmi}-clinically obese")
