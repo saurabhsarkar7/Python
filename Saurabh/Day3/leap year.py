72c29f57bcb4df699d86a01348d2742b621c46be
saurabhsarkar7 <saurabhsarkar7@gmail.com>
2024-06-24 14:21:28 +0530

    Leap year

---
+++ Saurabh/Day3/leap year.py
@@ -0,0 +1,13 @@
+year = int(input())
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a Leap Year")
+        else:
+            print(f"{year} is not a Leap Year")
+    else:
+        print(f"{year} is a Leap Year")
+else:
+    print(f"{year} is not a Leap Year")
+
+
