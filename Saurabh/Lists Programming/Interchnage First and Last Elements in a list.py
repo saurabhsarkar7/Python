7e0cdde442831b6fb492c313ae1533555deaf52e
saurabhsarkar7 <saurabhsarkar7@gmail.com>
2024-06-28 15:06:59 +0530

    Swapping Elements

--- Saurabh/Lists Programming/Interchnage First and Last Elements in a list.py
+++ Saurabh/Lists Programming/Interchnage First and Last Elements in a list.py
@@ -6,6 +6,8 @@
 #print(List)
 #list.reverse()
 #print(List)
+
+#Way 1
 def swapList(newList):
     size =len(newList)
 
@@ -18,6 +20,50 @@ def swapList(newList):
 #newList = [12,35,9,56,24]
 List = [12,35,9,56,24]  #after return, you can pass any variable name but ensure the same variable is passed in print.
 print(swapList(List))
+
+#way 2
+
+def swapList(newlist):
+    newlist[0], newlist[-1] = newlist[-1], newlist[0]
+    return newlist
+
+newlist = [12,35,9,56,24]
+print(swapList(newlist))
+
+#way3
+def swapList(newlist):
+    get = newlist[-1],newlist[0]
+    newlist[0],newlist[1] = get
+
+    return newlist
+
+newlist = [12, 35, 9, 56, 24]
+print(swapList(newlist))
+
+#way 4 - using * --assigns a list of item
+def swapList(newlist):
+    start,*middle,end =  newlist
+    newlist = [end,*middle,start]
+
+    return newlist
+
+newlist = [12, 35, 9, 56, 24]
+print(swapList(newlist))
+
+#way 5 -using pop and append
+def swapList(newlist):
+    first = newlist.pop(0)
+    last = newlist.pop(-1)
+
+    newlist.insert(0,last)
+    newlist.append(first)
+
+    return newlist
+
+newlist = [12, 35, 9, 56, 24]
+print(swapList(newlist))
+
+
 
 
 
